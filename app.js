c343b2044026fb9990a6060db930ab472fae95fe
zyx <1272861475@qq.com>
2017-12-28 23:49:29 +0800

    '1.0.0'

---
+++ app.js
@@ -0,0 +1,31 @@
+new Vue({
+    el:"#root",
+    data:{
+        datas:[
+            {
+                id:1,
+                title:'1111'
+            },
+            {
+                id:2,
+                title:'2222'
+            },
+            {
+                id:3,
+                title:'3333'
+            }
+        ],
+        state:false,
+        title:""
+    },
+    methods:{
+        aa(){
+            this.state=true;
+        },
+        bb(val){
+            this.title=val;
+            this.state=false;
+        }
+    }
+
+})
